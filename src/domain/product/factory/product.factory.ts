dee8ae84df1dfc61c367c79748ca429548c0c0d9
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2024-12-07 16:22:19 -0300

    feat: improve product factory

--- src/domain/product/factory/product.factory.ts
+++ src/domain/product/factory/product.factory.ts
@@ -3,17 +3,23 @@ import ProductInterface from "../entity/product.interface";
 import { v4 as uuid } from "uuid";
 import ProductB from "../entity/product-b";
 
+export enum EProduct {
+  A = "a",
+  B = "b",
+}
+
 export default class ProductFactory {
   public static create(
     type: string,
     name: string,
-    price: number
+    price: number,
+    id?: string
   ): ProductInterface {
     switch (type) {
-      case "a":
+      case EProduct.A:
         return new Product(uuid(), name, price);
-      case "b":
-        return new ProductB(uuid(), name, price);
+      case EProduct.B:
+        return new ProductB(id, name, price);
       default:
         throw new Error("Product type not supported");
     }
